fa42d77632ab4b138961824a2c3183340ff95b6f
phamquanghien <pqh.cntt@gmail.com>
2026-08-17 15:23:55 +0700

    MenuSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import HeroSection from '@/app/components/home/HeroSection';
+import MenuSection from './components/home/MenuSection';
 
 export default function HomePage() {
     return (
         <main>
             <HeroSection />
+            <MenuSection />
         </main>
     );
 }
